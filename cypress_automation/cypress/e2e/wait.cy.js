159be6b983694d6e79a8638d61ba2132fdc8a8b1
MikeNIke56 <111089840+MikeNIke56@users.noreply.github.com>
2026-08-29 19:02:38 -0400

    .

--- cypress_automation/cypress/e2e/wait.cy.js
+++
@@ -1,9 +0,0 @@
-describe("wait test", () => {
-    it("wait for default 4s", () => {
-        cy.once("uncaught:exception", () => false)
-        cy.visit('/')
-        cy.contains("Tests").click()
-        cy.contains("Web_Elements").click({force:true})
-        cy.get("#Button1").should("be.visible").click()
-    })
-})
